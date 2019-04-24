853f2cb4caedaf2ff07dfd977e2a83d80b1e9585
Eliezer Beczi <beczi_ezi@yahoo.com>
2019-04-24 03:52:53 +0300

    select visualized

--- src/Main.java
+++ src/Main.java
@@ -1,62 +1,10 @@
-import com.fasterxml.jackson.dataformat.xml.XmlMapper;
-import comm.Client;
 import comm.Server;
 import comm.Worker;
-import queries.misc.selectresultprotocol.Header;
-import queries.misc.selectresultprotocol.Page;
-import queries.misc.selectresultprotocol.Row;
-
-import java.io.*;
-import java.util.ArrayList;
-
 
 public class Main {
-
-    public static void main(String[] args) throws IOException {
-            Worker worker = new Worker();
-            Server server = new Server(1695, worker);
-            server.start();
-
-            Client.getClient().write("Use Emese" + "\n");
-            System.out.println(Client.getClient().readLine());
-
-            Client.getClient().write("select id,regular FROM Indextest2 where index1 = 1  AND index2=2 AND regular=3"+"\n");
-            System.out.println(Client.getClient().readLine());//read READY
-
-            XmlMapper xmlMapper = new XmlMapper();
-
-
-            Header readHeader = xmlMapper.readValue(Client.getClient().readLine(), Header.class);
-            for (int i = 0; i < readHeader.getPageNumber(); i++) {
-                Page page = xmlMapper.readValue(Client.getClient().readLine(), Page.class);
-                for (Row row : page.getRows()) {
-                    System.out.println(row.getValues());
-                }
-            }
-            System.out.println(Client.getClient().readLine());//read OK
-
-//          Client.getClient().write("DROP TABLE Indextest2"+"\n");
-//        System.out.println(Client.getClient().readLine());
-
-//            Client.getClient().write("CREATE TABLE Indextest2 ( id int PK , index1 int , index2 int , regular string )" + "\n");
-//            System.out.println(Client.getClient().readLine());
-//            int n = 1000000;
-//            for (int i = 1; i <= n; i++) {
-//                Client.getClient().write("insert into Indextest2 values ( " + i + " , " + i % 10 + " , " + i / 10 % 10 + " , " + i / 100 % 10 + "  )" + "\n");
-//            }
-//            for (int i = 1; i <= n; i++) {
-//                System.out.println(Client.getClient().readLine());
-//            }
-//
-//            Client.getClient().write("CREATE INDEX Indextest2.index1" + "\n");
-//            Client.getClient().write("CREATE INDEX Indextest2.index2" + "\n");
-//            System.out.println(Client.getClient().readLine());
-//            System.out.println(Client.getClient().readLine());
-
-//        for(int i=0;i<1000;i++){
-//            Client.getClient().write("delete from Indextest2 where id="+i+"\n");
-//            System.out.println(Client.getClient().readLine());
-//        }
+    public static void main(String[] args) {
+        Worker worker = new Worker();
+        Server server = new Server(1695, worker);
+        server.start();
     }
 }
-
--- src/gui/Controller.java
+++ src/gui/Controller.java
@@ -1,17 +1,24 @@
 package gui;
 
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import comm.Client;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyCodeCombination;
 import javafx.scene.input.KeyCombination;
-import javafx.util.Callback;
+import javafx.stage.Stage;
+import queries.misc.selectresultprotocol.Header;
+import queries.misc.selectresultprotocol.Page;
+import queries.misc.selectresultprotocol.Row;
 
+import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
@@ -26,13 +33,7 @@ public class Controller implements Initializable {
     public void initialize(URL url, ResourceBundle resourceBundle) {
         TreeItem<String> root = Client.getClient().getDatabases().visit();
         treeView.setRoot(root);
-
-        treeView.setCellFactory(new Callback<TreeView, TreeCell>() {
-            @Override
-            public TreeCell call(TreeView treeView) {
-                return new MyTreeCell();
-            }
-        });
+        treeView.setCellFactory(treeView -> new MyTreeCell());
 
         textArea = new TextArea();
         scrollPane.setContent(textArea);
@@ -53,37 +54,27 @@ public class Controller implements Initializable {
 
     private void setSaveAccelerator() {
         Scene scene = runButton.getScene();
-
-        scene.getAccelerators().put(new KeyCodeCombination(KeyCode.ENTER, KeyCombination.SHIFT_DOWN), new Runnable() {
-            @Override
-            public void run() {
-                runButton.fire();
-            }
-        });
-
-        scene.getAccelerators().put(new KeyCodeCombination(KeyCode.F5, KeyCombination.CONTROL_DOWN), new Runnable() {
-            @Override
-            public void run() {
-                refreshButton.fire();
-            }
-        });
-
-        scene.getAccelerators().put(new KeyCodeCombination(KeyCode.Q, KeyCombination.CONTROL_DOWN), new Runnable() {
-            @Override
-            public void run() {
-                closeMenu.fire();
-            }
-        });
-
-        scene.getAccelerators().put(new KeyCodeCombination(KeyCode.E, KeyCombination.CONTROL_DOWN), new Runnable() {
-            @Override
-            public void run() {
+        scene.getAccelerators().put(new KeyCodeCombination(KeyCode.ENTER, KeyCombination.SHIFT_DOWN), () -> runButton.fire());
+        scene.getAccelerators().put(new KeyCodeCombination(KeyCode.F5, KeyCombination.CONTROL_DOWN), () -> refreshButton.fire());
+        scene.getAccelerators().put(new KeyCodeCombination(KeyCode.Q, KeyCombination.CONTROL_DOWN), () -> closeMenu.fire());
+        scene.getAccelerators().put(new KeyCodeCombination(KeyCode.E, KeyCombination.CONTROL_DOWN), () -> {
+            try {
                 runSelectedLines();
+            } catch (IOException e) {
+                e.printStackTrace();
             }
         });
     }
 
-    private void runSelectedLines() {
+    private void runSelectedLines() throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("selectPopup.fxml"));
+        Scene scene = new Scene(loader.load());
+        Stage stage = new Stage();
+        stage.setScene(scene);
+        stage.setMinWidth(800);
+        stage.setMinHeight(700);
+        SelectController selectController = loader.getController();
+
         String sql = "";
 
         for (String line : textArea.getSelectedText().split("\\n")) {
@@ -95,16 +86,54 @@ public class Controller implements Initializable {
 
             if (processedLine.contains(";")) {
                 sql += processedLine.replaceFirst(".$", "").trim();
-
                 Client.getClient().write(sql + "\n");
-                String answer = Client.getClient().readLine();
 
-                if (!answer.equals("OK")) {
-                    Alert alert = new Alert(Alert.AlertType.ERROR, answer);
-                    alert.setTitle("Error Dialog");
-                    alert.setHeaderText(null);
-                    alert.showAndWait();
-                    break;
+                if (sql.toLowerCase().startsWith("select")) {
+                    String answer = Client.getClient().readLine();
+                    if (!answer.equals("READY")) {
+                        Alert alert = new Alert(Alert.AlertType.ERROR, answer);
+                        alert.setTitle("Error Dialog");
+                        alert.setHeaderText(null);
+                        alert.showAndWait();
+                        break;
+                    }
+
+                    XmlMapper xmlMapper = new XmlMapper();
+                    Header readHeader = xmlMapper.readValue(Client.getClient().readLine(), Header.class);
+                    ArrayList<ArrayList<String>> rows = new ArrayList<>();
+
+                    for (int i = 0; i < readHeader.getPageNumber(); i++) {
+                        Page page = xmlMapper.readValue(Client.getClient().readLine(), Page.class);
+                        for (Row row : page.getRows()) {
+                            rows.add(row.getValues());
+                        }
+                    }
+
+                    answer = Client.getClient().readLine();
+                    if (!answer.equals("OK")) {
+                        Alert alert = new Alert(Alert.AlertType.ERROR, answer);
+                        alert.setTitle("Error Dialog");
+                        alert.setHeaderText(null);
+                        alert.showAndWait();
+                        break;
+                    }
+
+                    // update window with new output
+                    selectController.load(readHeader.getColumnNames(), rows);
+
+                    if (!stage.isShowing()) {
+                        stage.show();
+                    }
+                } else {
+                    String answer = Client.getClient().readLine();
+
+                    if (!answer.equals("OK")) {
+                        Alert alert = new Alert(Alert.AlertType.ERROR, answer);
+                        alert.setTitle("Error Dialog");
+                        alert.setHeaderText(null);
+                        alert.showAndWait();
+                        break;
+                    }
                 }
 
                 sql = "";
@@ -118,7 +147,15 @@ public class Controller implements Initializable {
         setSaveAccelerator();
     }
 
-    public void runEverything(ActionEvent actionEvent) {
+    public void runEverything(ActionEvent actionEvent) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("selectPopup.fxml"));
+        Scene scene = new Scene(loader.load());
+        Stage stage = new Stage();
+        stage.setScene(scene);
+        stage.setMinWidth(800);
+        stage.setMinHeight(700);
+        SelectController selectController = loader.getController();
+
         String sql = "";
 
         for (String line : textArea.getText().split("\\n")) {
@@ -130,16 +167,54 @@ public class Controller implements Initializable {
 
             if (processedLine.contains(";")) {
                 sql += processedLine.replaceFirst(".$", "").trim();
-
                 Client.getClient().write(sql + "\n");
-                String answer = Client.getClient().readLine();
 
-                if (!answer.equals("OK")) {
-                    Alert alert = new Alert(Alert.AlertType.ERROR, answer);
-                    alert.setTitle("Error Dialog");
-                    alert.setHeaderText(null);
-                    alert.showAndWait();
-                    break;
+                if (sql.toLowerCase().startsWith("select")) {
+                    String answer = Client.getClient().readLine();
+                    if (!answer.equals("READY")) {
+                        Alert alert = new Alert(Alert.AlertType.ERROR, answer);
+                        alert.setTitle("Error Dialog");
+                        alert.setHeaderText(null);
+                        alert.showAndWait();
+                        break;
+                    }
+
+                    XmlMapper xmlMapper = new XmlMapper();
+                    Header readHeader = xmlMapper.readValue(Client.getClient().readLine(), Header.class);
+                    ArrayList<ArrayList<String>> rows = new ArrayList<>();
+
+                    for (int i = 0; i < readHeader.getPageNumber(); i++) {
+                        Page page = xmlMapper.readValue(Client.getClient().readLine(), Page.class);
+                        for (Row row : page.getRows()) {
+                            rows.add(row.getValues());
+                        }
+                    }
+
+                    answer = Client.getClient().readLine();
+                    if (!answer.equals("OK")) {
+                        Alert alert = new Alert(Alert.AlertType.ERROR, answer);
+                        alert.setTitle("Error Dialog");
+                        alert.setHeaderText(null);
+                        alert.showAndWait();
+                        break;
+                    }
+
+                    // update window with new output
+                    selectController.load(readHeader.getColumnNames(), rows);
+
+                    if (!stage.isShowing()) {
+                        stage.show();
+                    }
+                } else {
+                    String answer = Client.getClient().readLine();
+
+                    if (!answer.equals("OK")) {
+                        Alert alert = new Alert(Alert.AlertType.ERROR, answer);
+                        alert.setTitle("Error Dialog");
+                        alert.setHeaderText(null);
+                        alert.showAndWait();
+                        break;
+                    }
                 }
 
                 sql = "";
